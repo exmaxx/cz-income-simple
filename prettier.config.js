3916028ba3068a5675f14f41237b59a40954a29e
Marek Ulicny <xulicny@gmail.com>
2024-11-29 10:10:56 +0100

    chore: initial setup

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+export default {
+	useTabs: true,
+	trailingComma: 'es5',
+	semi: false,
+	singleQuote: true,
+	printWidth: 100,
+	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+	overrides: [
+		{
+			files: '*.html',
+			options: {
+				parser: 'html',
+			},
+		},
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte',
+			},
+		},
+	],
+}
