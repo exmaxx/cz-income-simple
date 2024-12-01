f45a0d793115676f9e75cb563e25c8708e6feee7
Marek Ulicny <xulicny@gmail.com>
2024-12-01 21:14:32 +0100

    chore: add vercel speed insights (#2)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.css'
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
+
+	injectSpeedInsights()
+
 	let { children } = $props()
 </script>
 
