86b9c9ba6f6d1f5be9ef0051cdc59c6d852743ec
Marek Ulicny <xulicny@gmail.com>
2024-12-06 17:22:19 +0100

    refactor: move introduction to Intro component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import IncomeResults from './IncomeResults.svelte'
 	import IncomeForm from './IncomeForm.svelte'
 	import { appState } from './store.svelte'
+	import Intro from './Intro.svelte'
 </script>
 
+<Intro />
 <IncomeForm />
 
 {#if appState.income.freelancer && appState.income.employee}
---
+++ src/routes/Intro.svelte
@@ -0,0 +1,14 @@
+<div class="flex flex-col items-center">
+	<h1
+		class=" text-4xl mx-2 my-1 bg-gradient-to-tl from-primary via-secondary to-secondary bg-clip-text
+		text-center font-medium uppercase tracking-wide text-transparent dark:bg-gradient-to-br"
+	>
+		Kalkulátor vašeho příjmu
+	</h1>
+
+	<h2 class="order-first mb-2 mt-10 text-base">Pro zaměstnance i OSVČ</h2>
+
+	<p class="m-10 text-center">
+		Zjistěte jednoduše, jaký <strong class="font-black">hrubý příjem</strong> potřebujete, abyste si splnili své <strong>představy o čistém výdělku</strong>.
+	</p>
+</div>
