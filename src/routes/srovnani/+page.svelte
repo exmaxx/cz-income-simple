405e80f10e83a98fff0b67ea3debd472343a488d
Marek Ulicny <xulicny@gmail.com>
2024-12-08 00:12:10 +0100

    feat: add hover highlight to overview table

--- src/routes/srovnani/+page.svelte
+++ src/routes/srovnani/+page.svelte
@@ -25,49 +25,49 @@
 			</thead>
 
 			<tbody>
-				<tr>
+				<tr class="hover">
 					<th>Čistý příjem</th>
 					<CellCurrency yearlyAmount={freelancer.net} period="hour" />
 					<CellCurrency yearlyAmount={employee.net} period="hour" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th></th>
 					<CellCurrency yearlyAmount={freelancer.net} period="day" />
 					<CellCurrency yearlyAmount={employee.net} period="day" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th></th>
 					<CellCurrency yearlyAmount={freelancer.net} period="month" />
 					<CellCurrency yearlyAmount={employee.net} period="month" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th></th>
 					<CellCurrency yearlyAmount={freelancer.net} period="year" />
 					<CellCurrency yearlyAmount={employee.net} period="year" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Hrubý příjem</th>
 					<CellCurrency yearlyAmount={freelancer.gross} period="hour" />
 					<CellCurrency yearlyAmount={employee.gross} period="hour" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th></th>
 					<CellCurrency yearlyAmount={freelancer.gross} period="day" />
 					<CellCurrency yearlyAmount={employee.gross} period="day" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th></th>
 					<CellCurrency yearlyAmount={freelancer.gross} period="month" />
 					<CellCurrency yearlyAmount={employee.gross} period="month" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th></th>
 					<CellCurrency yearlyAmount={freelancer.gross} period="year" />
 					<CellCurrency yearlyAmount={employee.gross} period="year" />
@@ -77,7 +77,7 @@
 					<th colspan="3" class="text-accent">Daně</th>
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Daň z příjmu</th>
 					<CellCurrency yearlyAmount={freelancer.contributions.incomeTax} period="month" />
 					<CellCurrency yearlyAmount={employee.contributions.incomeTax} period="month" />
@@ -87,19 +87,19 @@
 					<th colspan="3" class="text-accent">Socialní pojištění</th>
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Já</th>
 					<CellCurrency yearlyAmount={freelancer.contributions.social} period="month" />
 					<CellCurrency yearlyAmount={employee.contributions.social.employee} period="month" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Zaměstnavatel</th>
 					<td>-</td>
 					<CellCurrency yearlyAmount={employee.contributions.social.employer} period="month" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Dohromady</th>
 					<CellCurrency yearlyAmount={freelancer.contributions.social} period="month" />
 					<CellCurrency
@@ -113,19 +113,19 @@
 					<th colspan="3" class="text-accent">Zdravotní pojištění</th>
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Já</th>
 					<CellCurrency yearlyAmount={freelancer.contributions.health} period="month" />
 					<CellCurrency yearlyAmount={employee.contributions.health.employee} period="month" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Zaměstnavatel</th>
 					<td>-</td>
 					<CellCurrency yearlyAmount={employee.contributions.health.employer} period="month" />
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Dohromady</th>
 					<CellCurrency yearlyAmount={freelancer.contributions.health} period="month" />
 					<CellCurrency
@@ -139,7 +139,7 @@
 					<th colspan="3" class="text-accent">Celkové náklady pro klienta / zaměstnavatele</th>
 				</tr>
 
-				<tr>
+				<tr class="hover">
 					<th>Náklady</th>
 					<CellCurrency yearlyAmount={freelancer.gross} period="month" />
 					<CellCurrency
