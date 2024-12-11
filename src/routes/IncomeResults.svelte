36a35e5803fb34057b834fb84e5b565061b4b1a3
Marek Ulicny <xulicny@gmail.com>
2024-12-11 20:15:27 +0100

    feat: scroll to results

--- src/routes/IncomeResults.svelte
+++ src/routes/IncomeResults.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
-	import type { Result } from './types'
+	import { tick } from 'svelte'
 	import { fly } from 'svelte/transition'
+	import type { Result } from './types'
 	import IncomeResultsFreelancer from './IncomeResultsFreelancer.svelte'
 	import IncomeResultsEmployee from './IncomeResultsEmployee.svelte'
 	import IncomeResultsClient from './IncomeResultsClient.svelte'
 	import IncomeResultsEmployer from './IncomeResultsEmployer.svelte'
 
 	let { freelancer, employee }: Result = $props()
+	let elem: HTMLDivElement
+
+	$effect.pre(() => {
+		tick().then(() => {
+			elem.scrollIntoView({ behavior: 'smooth' })
+		})
+	})
 </script>
 
-<div class="flex flex-col items-center" in:fly={{ y: -20 }}>
+<div class="flex flex-col items-center" in:fly={{ y: -20 }} bind:this={elem}>
 	<h2>Hrubý příjem</h2>
 
 	<IncomeResultsFreelancer results={freelancer} />
